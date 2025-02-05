fbacf0ad98ebf310e381cd17612cb19839106ec2
Vlad1m1rKolebakin <kolebakin.v@yandex.ru>
2025-02-05 07:52:29 +0300

    first commit

---
+++ ScalingSquaredStrings.js
@@ -0,0 +1,6 @@
+//7kyu Scaling Squared Strings
+function scale(strng, k, n) {
+	const arr=strng.split('\n').map(v=>v.split('').map(v=>(v).repeat(k)).join(''))
+	.map(v=>(v+' ').repeat(n)).map(v=>v.trim().replace(/ /g,'\n')).join('\n')
+	return arr
+  }
