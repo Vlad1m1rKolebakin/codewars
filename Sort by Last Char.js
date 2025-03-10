295ade602ff685956b0cd5111342c47034c25dd7
Vlad1m1rKolebakin <kolebakin.v@yandex.ru>
2025-03-10 14:54:37 +0300

    fix

--- Sort by Last Char.js
+++ Sort by Last Char.js
@@ -1,15 +1,16 @@
 function last(str) {
+    // Split the string into an array of words
     let words = str.split(' ');
   
-
+    // Sort words based on their last character
     return words.sort((a, b) => {
-      const lastCharA = a[a.length - 1];
-      const lastCharB = b[b.length - 1];
+      const lastCharA = a[a.length - 1]; // last character of word a
+      const lastCharB = b[b.length - 1]; // last character of word b
   
-
+      // Compare last characters
       if (lastCharA < lastCharB) return -1;
-      if (lastCharA > lastCharB) return 1;Ё
-      return 0;
+      if (lastCharA > lastCharB) return 1;
+      return 0; // If last characters are equal, maintain the original order
     });
   }
   
